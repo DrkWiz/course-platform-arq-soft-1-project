9b398c64f101738279402ee1f77554991096cb32
emi <emilianobarg@gmail.com>
2024-05-03 18:27:23 -0300

    ARREGLE TODO

--- backend/router/router.go
+++ backend/router/router.go
@@ -1,9 +1,11 @@
 package router
 
 import (
+	"backend/controllers/users"
+
 	"github.com/gin-gonic/gin"
 )
 
 func MapUrls(engine *gin.Engine) {
-	engine.POST("/users/login", users.login)
+	engine.POST("/users/login", users.Login)
 }
