083a18d272fb71a5bcd54c254d01a28f816f061b
Francisco <2214038@ucc.edu.ar>
2024-05-03 18:23:00 -0300

    solo dios sabe que es <<<<<<<<<<<<<head

--- backend/router/router.go
+++ backend/router/router.go
@@ -1,11 +1,9 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func MapUrls(engine *gin.Engine) {
-	engine.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	engine.POST("/users/login", users.login)
 }
