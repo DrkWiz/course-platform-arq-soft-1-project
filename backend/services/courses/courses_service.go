b942a14123e9221637f3738b633abb05ed1f7c05
Nicolas <nicolasantiagomez4@gmail.com>
2024-05-14 15:23:34 -0300

    no se que es este cambio

--- backend/services/courses/courses_service.go
+++ backend/services/courses/courses_service.go
@@ -1,1 +1,10 @@
 package courses
+
+import coursesDomain "backend/domain/courses"
+
+func CreateCourse(request coursesDomain.CourseCreateRequest) coursesDomain.CreateCourseResponse {
+
+	//create in db
+
+	return coursesDomain.CreateCourseResponse{ID: 1, Name: request.Name, Description: request.Description, Price: request.Price, ClassAmount: request.ClassAmount, StartDate: request.StartDate, EndDate: request.EndDate}
+}
