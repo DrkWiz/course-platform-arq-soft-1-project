cf86c4c11bf8d521c5e6ef5115b6349431f8fb69
emi <emilianobarg@gmail.com>
2024-05-20 16:52:10 -0300

    attempt at createuser

--- backend/services/users/users_service.go
+++ backend/services/users/users_service.go
@@ -13,7 +13,7 @@ import (
 type usersService struct{}
 
 type usersServiceInterface interface {
-	GetStudentById(id int) (dto.StudentMinDto, e.ApiError)
+	GetUserById(id int) (dto.StudentMinDto, e.ApiError)
 }
 
 var (
@@ -24,9 +24,9 @@ func init() {
 	UsersService = &usersService{}
 }
 
-func (s *usersService) GetStudentById(id int) (dto.StudentMinDto, e.ApiError) {
+func (s *usersService) GetUserById(id int) (dto.StudentMinDto, e.ApiError) {
 
-	log.Print("GetStudentById: ", id)
+	log.Print("GetUserById: ", id)
 
 	var student usersModel.User = usersClient.GetUserById(id)
 	var studentMinDto dto.StudentMinDto
@@ -38,8 +38,8 @@ func (s *usersService) GetStudentById(id int) (dto.StudentMinDto, e.ApiError) {
 	return studentMinDto, nil
 }
 
-// GetStudentById method is not working yet because it is not implemented in the database yet
+// GetUseById method is not working yet because it is not implemented in the database yet
 
-func GetStudentById(id int) usersModel.User {
+func GetUserById(id int) usersModel.User {
 	return usersModel.User{IdUser: id}
 }
