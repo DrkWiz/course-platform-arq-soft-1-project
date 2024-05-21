f53f86fc4c082d9396806ed792a45a585c2b8418
Francisco <2214038@ucc.edu.ar>
2024-05-21 13:13:22 -0300

    coomentarios

--- backend/controllers/courses/courses_controller.go
+++ backend/controllers/courses/courses_controller.go
@@ -9,8 +9,8 @@ import (
 )
 
 func CreateCourse(c *gin.Context) {
-	var createCourseRequest coursesDomain.CourseCreateRequest
-	c.BindJSON(&createCourseRequest)
-	response := coursesService.CreateCourse(createCourseRequest)
-	c.JSON(http.StatusCreated, response)
+	var createCourseRequest coursesDomain.CourseCreateRequest    // aca se crea la variable createCourseRequest de tipo CourseCreateRequest
+	c.BindJSON(&createCourseRequest)                             // aca se bindea el json que viene en el request a la variable createCourseRequest
+	response := coursesService.CreateCourse(createCourseRequest) // aca se llama a la funcion CreateCourse del paquete coursesService y se le pasa como parametro la variable createCourseRequest
+	c.JSON(http.StatusCreated, response)                         // aca se responde con un json con el status 201 y la variable response
 }
--- backend/controllers/users/users_controller.go
+++ backend/controllers/users/users_controller.go
@@ -11,8 +11,8 @@ import (
 func Login(c *gin.Context) {
 	//validate with db
 
-	var loginRequest usersDomain.LoginRequest
-	c.BindJSON(&loginRequest)
-	response := usersService.Login(loginRequest)
-	c.JSON(http.StatusOK, response)
+	var loginRequest usersDomain.LoginRequest    // aca se crea la variable loginRequest de tipo LoginRequest
+	c.BindJSON(&loginRequest)                    // aca se bindea el json que viene en el request a la variable loginRequest
+	response := usersService.Login(loginRequest) // aca se llama a la funcion Login del paquete usersService y se le pasa como parametro la variable loginRequest
+	c.JSON(http.StatusOK, response)              // aca se responde con un json con el status 200 y la variable response
 }
