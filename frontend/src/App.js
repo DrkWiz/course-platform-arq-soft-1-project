1c24b70a71a2baa0f76552de45903a348ba62def
Francisco <2214038@ucc.edu.ar>
2024-05-22 18:11:24 -0300

    cree menu login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+//agrego imports para crear un boton que me lleve al login.js
+import "./components/Login.js";
+import "./components/login.css";
 
 function App() {
+  const openLogin = () => {
+    window.open("/login", "_blank", "noopener,noreferrer");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+
+      <div className="BarraSuperiorFlotante">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={openLogin}> Login</button>
+        <button onClick={openLogin}> Login</button>
+        <button onClick={openLogin}> Login</button>
+        <button onClick={openLogin}> Login</button>
+        <button onClick={openLogin}> Login</button>
+        <button onClick={openLogin}> Login</button>
+        <button onClick={openLogin}> Login</button>
+        <button onClick={openLogin}> Login</button>
+        <button onClick={openLogin}> Login</button>
+        <button onClick={openLogin}> Login</button>
+        <button onClick={openLogin}> Login</button>
+        <button onClick={openLogin}> Login</button>
+
+      </div>
+
     </div>
   );
 }
