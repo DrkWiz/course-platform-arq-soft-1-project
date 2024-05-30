e09913110abe09ca9a18d0aa83138d0e85cbd028
emi <emilianobarg@gmail.com>
2024-05-30 14:09:15 -0300

    header for main menu and login frontend backend

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const response = await fetch("http://localhost:8080/login", {
+    const response = await fetch("http://localhost:8080/users/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
