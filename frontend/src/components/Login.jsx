35fa7c5db390ca1762a6d962dcecef14326c6125
DrkWiz <129195663+DrkWiz@users.noreply.github.com>
2024-05-29 21:31:28 -0300

    arreglo menu login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -5,24 +5,21 @@ import Section from "./Section";
 const Login = () => {
   return (
     <Section
-      className="pt-[12rem] -mt-[5.25rem]"
-      crosses
-      crossesOffset="lg:translate-y-[5.25rem]"
+      className="-mt-[5.25rem]"
       customPaddings>
-      <div className="flex justify-center items-center h-screen">
-        <div className="p-8 rounded-lg shadow-lg max-w-md w-full bg-gray-100">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-4">Login</h2>
-          <form className="space-y-4">
+      <div className="flex justify-center items-center h-screen ">
+        <div className="p-8 rounded-lg shadow-lg max-w-md w-full bg-gray-800">
+          <form className="space-y-4 font-semibold">
             <div>
-              <label htmlFor="username" className="block text-gray-800">Username:</label>
+              <label htmlFor="username" className="block text-withe">Username:</label>
               <InputField type="text" id="username" name="username" className="w-full" />
             </div>
             <div>
-              <label htmlFor="password" className="block text-gray-800">Password:</label>
+              <label htmlFor="password" className="block text-withe">Password:</label>
               <InputField type="password" id="password" name="password" className="w-full" />
             </div>
             <div className="rounded bg-gray-800">
-              <Button type="submit" className="w-full bg-gray-800 text-white hover:bg-gray-800 rounded">Login</Button>
+              <Button type="submit" className="w-full bg-gray-800 text-white hover:bg-gray-800 rounded text-2xl font-semibold">Login</Button>
             </div>
           </form>
         </div>
